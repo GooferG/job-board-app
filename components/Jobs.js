6db9db4dd2189054b69d1c8c47cfcb1839209585
Luiz M <luiz.meneghim@hotmail.com>
2022-06-28 17:59:49 -0700

    adding the jobs to the home page with components

---
+++ components/Jobs.js
@@ -0,0 +1,14 @@
+import Job from 'components/Job';
+
+const Jobs = ({ jobs }) => {
+  if (!jobs) return null;
+  return (
+    <>
+      {jobs.map((job, index) => (
+        <Job key={index} job={job} />
+      ))}
+    </>
+  );
+};
+
+export default Jobs;
