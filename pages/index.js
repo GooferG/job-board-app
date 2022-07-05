1d6700958a18c66bf75305325469bd88799df03a
Luiz M <luiz.meneghim@hotmail.com>
2022-07-04 19:59:52 -0700

    finishing setup for company view when logged in

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import prisma from 'lib/prisma';
-import { getJobs } from 'lib/data';
+import { getJobs, getUser } from 'lib/data';
 import Jobs from 'components/Jobs';
-import { useSession } from 'next-auth/react';
+import { useSession, getSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
 export default function Home({ jobs }) {
@@ -25,18 +25,58 @@ export default function Home({ jobs }) {
           login
         </a>
       )}
+
+      {session && (
+        <>
+          <p className="mb-10 text-2xl font-normal">
+            Welcome, {user.name}
+            {user.company && (
+              <span className="bg-black text-white uppercase text-sm p-2">
+                Company
+              </span>
+            )}
+          </p>
+          {user.company ? (
+            <>
+              <button className="border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black ">
+                click here to post a new job
+              </button>
+              <button className="ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black ">
+                see all the jobs you posted
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black ">
+                see all the jobs you applied to
+              </button>
+            </>
+          )}
+        </>
+      )}
       <Jobs jobs={jobs} />
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
   let jobs = await getJobs(prisma);
   jobs = JSON.parse(JSON.stringify(jobs));
+
+  if (!session) {
+    return {
+      props: { jobs },
+    };
+  }
+  let user = await getUser(session.user.id, prisma);
+  user = JSON.parse(JSON.stringify(user));
 
   return {
     props: {
       jobs,
+      user,
     },
   };
 }
